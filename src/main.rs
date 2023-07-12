0820cd690af0b5c1127b109714ff633426a72cd3
rohit <prkteja99@gmail.com>
2023-07-12 12:16:37 +0530

    implement stdin->single_client stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+
+use std::net::SocketAddr;
+use tower_http::trace::{DefaultMakeSpan, TraceLayer};
+
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+use std::io::Write;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "pubsub=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new()
+        .route("/ws", get(ws_handler))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::default().include_headers(true)),
+        );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+        .await
+        .unwrap();
 }
+
+async fn ws_handler( ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    loop {
+        let mut msg = String::new();
+        print!("> ");
+        std::io::stdout().flush().unwrap();
+        let num_bytes = std::io::stdin().read_line(&mut msg).unwrap(); //keeps newline at the end
+        if num_bytes <= 1 {
+            println!("exiting");
+            return;
+        }
+        if socket.send(Message::Text(msg)).await.is_err() {
+            println!("client abruptly disconnected");
+            return;
+        }
+    }
+}
+
