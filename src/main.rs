566eedcea5407ec8d388e1747b9ec5648ae6c917
rohit <prkteja99@gmail.com>
2023-07-20 14:52:21 +0530

    implement single channel multi client communication

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use axum::{
-    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    extract::{State, ws::{WebSocket, WebSocketUpgrade}},
     response::IntoResponse,
     routing::get,
     Router,
@@ -10,7 +10,11 @@ use tower_http::trace::{DefaultMakeSpan, TraceLayer};
 
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-use std::io::Write;
+mod channel;
+mod client;
+use channel::Channel;
+use client::Subscriber;
+use client::Publisher;
 
 #[tokio::main]
 async fn main() {
@@ -22,8 +26,11 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    let chan = Channel::new(32);
     let app = Router::new()
-        .route("/ws", get(ws_handler))
+        .route("/sub", get(sub_handler))
+        .route("/pub", get(pub_handler))
+        .with_state(chan)
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::default().include_headers(true)),
@@ -32,29 +39,30 @@ async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
-        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+        .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-async fn ws_handler( ws: WebSocketUpgrade) -> impl IntoResponse {
-    ws.on_upgrade(handle_socket)
+async fn sub_handler(ws: WebSocketUpgrade, State(chan): State<Channel>) -> impl IntoResponse {
+    ws.on_upgrade(move |socket| handle_socket(socket, chan, client::ClientRole::Subscriber))
+}
+async fn pub_handler(ws: WebSocketUpgrade, State(chan): State<Channel>) -> impl IntoResponse {
+    ws.on_upgrade(move |socket| handle_socket(socket, chan, client::ClientRole::Publisher))
 }
 
-async fn handle_socket(mut socket: WebSocket) {
-    loop {
-        let mut msg = String::new();
-        print!("> ");
-        std::io::stdout().flush().unwrap();
-        let num_bytes = std::io::stdin().read_line(&mut msg).unwrap(); //keeps newline at the end
-        if num_bytes <= 1 {
-            println!("exiting");
-            return;
-        }
-        if socket.send(Message::Text(msg)).await.is_err() {
-            println!("client abruptly disconnected");
-            return;
+async fn handle_socket(socket: WebSocket, chan: Channel, role: client::ClientRole) {
+    match role {
+        client::ClientRole::Publisher => {
+            // publish here
+            let mut publ: Publisher = Publisher::new(socket, chan.id, chan.tx);
+            publ.attach().await;
+        },
+        client::ClientRole::Subscriber => {
+            let mut sub: Subscriber = Subscriber::new(socket, chan.id, chan.rx);
+            sub.attach().await;
         }
     }
 }
+
 
