83f2d19d799fbcfe5f61abdf8817a346e98b93cb
Rohit <prkteja99@gmail.com>
2023-08-14 21:10:01 +0530

    add dockerfile

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ async fn main() {
                 .make_span_with(DefaultMakeSpan::default().include_headers(true)),
         );
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
